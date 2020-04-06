301820307b542f43e5606b7d1fc7edcf3f90d3da
AislynYC <chuang.yuchun@gmail.com>
2020-04-06 19:55:34 +0800

    Create React environment

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+const App = () => {
+  // codes...
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        // 改為babel才可正確轉換
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};
